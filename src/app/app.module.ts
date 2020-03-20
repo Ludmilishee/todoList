fc3b805bc478acc8065599e60ad94f579c115766
Ludmilishee <luda_babich@mail.ru>
2020-03-21 02:34:27 +0500

    card CRUD draft colution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardListComponent } from './card-list/card-list.component';
-import { CardComponent } from './card-list/card/card.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { CardFormComponent } from './card-list/card-form/card-form.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardListComponent,
-    CardComponent
+    CardFormComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    DragDropModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatDialogModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Card } from './shared/card.model';
+import { MatDialog } from '@angular/material/dialog';
+import { CardFormComponent } from './card-form/card-form.component';
+
 
 @Component({
   selector: 'tl-card-list',
@@ -7,9 +12,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardListComponent implements OnInit {
 
-  constructor() { }
+  isEditMode: boolean;
+  cards: Card[] = [
+    {
+      id: 1,
+      name: 'AAA',
+      description: `Description AAA`,
+      date: new Date()
+    },
+    {
+      id: 2,
+      name: 'BBB',
+      description: 'Description BBB',
+      date: new Date()
+    },
+    {
+      id: 3,
+      name: 'CCC',
+      description: 'Description CCC',
+      date: new Date()
+    }
+  ];
+
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  onCardEdit(card: Card, e: Event) {
+    console.log(card);
+    e.stopImmediatePropagation();
+    this.openDialog();
+  }
+
+  onCardDelete(card: Card, e: Event) {
+    console.log(card);
+    e.stopImmediatePropagation();
+    this.cards = this.cards.filter(c => c.id !== card.id);
+  }
+
+  onCardDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.cards, event.previousIndex, event.currentIndex);
+  }
+
+  onEditModeChange(checked: boolean) {
+    this.isEditMode = checked;
+  }
+
+  private openDialog(): void {
+    const dialogRef = this.dialog.open(CardFormComponent, {
+      width: '250px',
+      data: {name: 'a', animal: 'b'}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed ' + result);
+    });
+  }
 }
