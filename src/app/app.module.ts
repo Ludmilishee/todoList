78b4e2ea181c45721ef42a09ed68687f3c8bf07d
Ludmilishee <luda_babich@mail.ru>
2020-03-20 20:24:58 +0500

    add material, create primary structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardListComponent } from './card-list/card-list.component';
+import { CardComponent } from './card-list/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardListComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
